bcf9b8cd4c481366091bbbe1eb5d81820fb43125
Zhantes <zhantes3@gmail.com>
2025-07-05 15:42:34 -0300

    Add blank username error message

--- aqchat/settings.py
+++ aqchat/settings.py
@@ -63,7 +63,7 @@ def page_settings():
         gh_user = st.text_input("Your Github Username :red[*]", value=config.get("gh_user", ""))
         gh_token = st.text_input(f"Github PAT", help="Personal Access Token, only required for private repositories.", value=config.get("gh_token", ""), type="password") # TODO: Find a way to align help tooltip so it's closer to label. Also add instructions on how to find the PAT, I couldn't make the tooltip multi-line.
         saved = st.form_submit_button("Save")
-        if saved:
+        if saved and len(f"{gh_user}") != 0 and len(f"{repo_url}") != 0:
             try:
                 extract_repo_name(f"{repo_url}")
             except:
@@ -74,3 +74,5 @@ def page_settings():
                 config["gh_token"] = gh_token
                 st.success("Settings saved! Please refresh the app to fully apply changes.")
                 save_config()
+        else:
+            st.error("Please fill all required fields.")
