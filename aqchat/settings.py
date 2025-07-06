9f442be6b8d98e9dd8aa45fd7a48e2f2b751e111
Zhantes <zhantes3@gmail.com>
2025-07-06 08:57:27 -0300

    Fix blank required fields error message

--- aqchat/settings.py
+++ aqchat/settings.py
@@ -63,16 +63,17 @@ def page_settings():
         gh_user = st.text_input("Your Github Username :red[*]", value=config.get("gh_user", ""))
         gh_token = st.text_input("Github PAT", help="Personal Access Token, only required for private repositories.", value=config.get("gh_token", ""), type="password") # TODO: Find a way to align help tooltip so it's closer to label. Also add instructions on how to find the PAT, I couldn't make the tooltip multi-line.
         saved = st.form_submit_button("Save")
-        if saved and gh_user and repo_url:
-            try:
-                extract_repo_name(repo_url)
-            except:
-                st.error("Invalid repository URL, please verify the URL and try again.")
+        if saved:
+            if repo_url and gh_user:
+                try:
+                    extract_repo_name(repo_url)
+                except:
+                    st.error("Invalid repository URL, please verify the URL and try again.")
+                else:
+                    config["repo_url"] = repo_url
+                    config["gh_user"] = gh_user
+                    config["gh_token"] = gh_token
+                    st.success("Settings saved! Please refresh the app to fully apply changes.")
+                    save_config()
             else:
-                config["repo_url"] = repo_url
-                config["gh_user"] = gh_user
-                config["gh_token"] = gh_token
-                st.success("Settings saved! Please refresh the app to fully apply changes.")
-                save_config()
-        else:
-            st.error("Please fill all required fields.")
+                st.error("Please fill all required fields.")
