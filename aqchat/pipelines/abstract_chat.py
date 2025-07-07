131c94ddaea0ba58db4bfbce587951fdb0ff9eb8
Jacob Farnsworth <6502enthusiast@gmail.com>
2025-07-07 19:15:50 +0200

    Fix chat pipeline interface

--- aqchat/pipelines/abstract_chat.py
+++ aqchat/pipelines/abstract_chat.py
@@ -2,8 +2,6 @@ from typing import Dict, List, Iterator
 
 from langchain_core.messages.base import BaseMessageChunk
 
-from pipelines.abstract_memory import AbstractMemoryPipeline
-
 class AbstractChatPipeline:
     """This interface represents an LLM chat pipeline which can be
     queried with a memory interface and a current message list.
@@ -17,7 +15,7 @@ class AbstractChatPipeline:
     # PUBLIC API
     # --------------------------------------------------------------
 
-    def query(self, memory: AbstractMemoryPipeline, messages: List[Dict[str, str]]) -> Iterator[BaseMessageChunk]:
+    def query(self, messages: List[Dict[str, str]]) -> Iterator[BaseMessageChunk]:
         """Stream an answer for *messages*.
 
         ``messages`` must be a list of chat messages of the form
